fc2cf5278c952b93ddfec3a80f167d98937c543b
neoweileng-spec <neoweileng@gmail.com>
2025-08-29 15:56:38 +0800

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from http.server import BaseHTTPRequestHandler
 
-@app.route("/", methods=["GET"])
-def ping():
-    return "ok"
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"ok")
